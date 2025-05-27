fdd92fedd2c1e8e6048628f85776a16a1b7b3308
aBugInTheSystem <ekom338.com@gmail.com>
2025-05-27 12:40:54 -0400

    Menu draft

--- src/main/java/bankingSystem/ekom/Main.java
+++ src/main/java/bankingSystem/ekom/Main.java
@@ -2,6 +2,32 @@ package bankingSystem.ekom;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("********");
+        System.out.println("Banking Program");
+        System.out.println("********");
+        boolean userSearching = true;
+
+
+        while(userSearching){
+            System.out.println("1. Create an Account.");
+            System.out.print("Selection: ");
+            int userOption = sc.nextInt();
+
+          switch (userOption){
+              case 1:
+                  System.out.println("Create an account");
+                  break;
+
+              default:
+                  System.out.println("Exiting");
+                  userSearching = false;
+
+          }
+
+        }
+
+
 
     }
 }
